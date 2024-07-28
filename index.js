9ed70b99ecca6b1fcd22a49c729722fca5d8a3ba
Anushree mehta <104919688+anushreemehta6@users.noreply.github.com>
2024-07-29 00:20:40 +0530

    js file added

---
+++ index.js
@@ -0,0 +1,12 @@
+ let button = document.getElementById("btn")
+
+function Change(){
+    button.style.backgroundColor = '#DAB5C4'
+    button.style.color = 'black'
+}
+function change2(){
+    button.style.backgroundColor ='#917468'
+    button.style.color = 'white'
+    
+
+}
