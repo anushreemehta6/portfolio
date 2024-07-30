941b81ab8bb787687fe78a8d25f7f101fc696d1c
Anushree mehta <104919688+anushreemehta6@users.noreply.github.com>
2024-07-30 09:29:28 +0530

    functionality added

--- index.js
+++ index.js
@@ -11,3 +11,42 @@ function change2(){
     
 
 }
+
+//more button on about me page 
+
+let button2 = document.getElementById("more");
+
+function hover(){
+    button2.style.backgroundColor = '#917468'
+    button2.style.color = 'black'
+}
+function hovertwo(){
+    button2.style.backgroundColor ='#2D3551'
+    button2.style.color = 'white'
+    
+
+}
+
+// increase in the img size 
+
+let myImg = document.getElementById("pic2")
+function zoomin(){
+     
+        myImg.style.height = '469px';
+       myImg.style.width = '469px';
+    
+}
+function zoomout(){
+    myImg.style.height = '376px';
+       myImg.style.width = '376px';
+    
+
+}
+
+// scroll to experience
+document.getElementById('exp').addEventListener('click', function() {
+    document.getElementById('heading-e').scrollIntoView({ behavior: 'smooth' });
+  });
+  document.getElementById('skl').addEventListener('click', function() {
+    document.getElementById('heading-e').scrollIntoView({ behavior: 'smooth' });
+  });
