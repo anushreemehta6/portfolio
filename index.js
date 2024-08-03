d7b22676e4446655c13866b804d9925218fc9e4a
Anushree mehta <104919688+anushreemehta6@users.noreply.github.com>
2024-08-03 15:18:53 +0530

    js added

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
- // about me button on main screen 
- let button = document.getElementById("btn")
+// about me button on main screen
+let button = document.getElementById("btn");
 
-function Change(){
-    button.style.backgroundColor = '#E6632B'
-    button.style.color = 'black'
+function Change() {
+  button.style.backgroundColor = "#E6632B";
+  button.style.color = "black";
 }
-function change2(){
-    button.style.backgroundColor ='#917468'
-    button.style.color = 'white'
-    
-
+function change2() {
+  button.style.backgroundColor = "#917468";
+  button.style.color = "white";
 }
 
-//more button on about me page 
+let butn = document.getElementById("rbtn");
+function change3() {
+  butn.style.backgroundColor = "#E6632B";
+  butn.style.color = "black";
+}
+function change4() {
+  butn.style.backgroundColor = "#917468";
+  butn.style.color = "white";
+}
+
+//more button on about me page
 
 let button2 = document.getElementById("more");
 
-function hover(){
-    button2.style.backgroundColor = '#917468'
-    button2.style.color = 'black'
+function hover() {
+  button2.style.backgroundColor = "#917468";
+  button2.style.color = "black";
 }
-function hovertwo(){
-    button2.style.backgroundColor ='#2D3551'
-    button2.style.color = 'white'
-    
-
+function hovertwo() {
+  button2.style.backgroundColor = "#2D3551";
+  button2.style.color = "white";
 }
 
-// increase in the img size 
+// increase in the img size
 
-let myImg = document.getElementById("pic2")
-function zoomin(){
-     
-        myImg.style.height = '469px';
-       myImg.style.width = '469px';
-    
+let myImg = document.getElementById("pic2");
+function zoomin() {
+  myImg.style.height = "469px";
+  myImg.style.width = "469px";
 }
-function zoomout(){
-    myImg.style.height = '376px';
-       myImg.style.width = '376px';
-    
-
+function zoomout() {
+  myImg.style.height = "376px";
+  myImg.style.width = "376px";
 }
 
-
-document.getElementById('exp').addEventListener('click', function() {
-    document.getElementById('heading-e').scrollIntoView({ behavior: 'smooth' });
-  });
+document.getElementById("exp").addEventListener("click", function () {
+  document.getElementById("heading-e").scrollIntoView({ behavior: "smooth" });
+});
 // scroll to skill
-  document.getElementById('skl').addEventListener('click', function() {
-    document.getElementById('heading-e').scrollIntoView({ behavior: 'smooth' });
-  });
+document.getElementById("skl").addEventListener("click", function () {
+  document.getElementById("heading-e").scrollIntoView({ behavior: "smooth" });
+});
 
-  // scroll to contact me 
-  document.getElementById('cont').addEventListener('click', function() {
-    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
-  });
-  
+// scroll to contact me
+document.getElementById("cont").addEventListener("click", function () {
+  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+});
 
+// document.getElementById('exp-2').addEventListener('click', function() {
+//   document.getElementById('more').scrollIntoView({ behavior: 'smooth' });
+// });
 
-
-  document.getElementById('exp-2').addEventListener('click', function() {
-    document.getElementById('more').scrollIntoView({ behavior: 'smooth' });
-  });
+//scroll to projects
+document.getElementById("pj").addEventListener("click", function () {
+  document.getElementById("heading-p").scrollIntoView({ behavior: "smooth" });
+});
